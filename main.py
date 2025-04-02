0a9614964c68d300e50a1e89fc08aea79aba341f
Josh Mercik <josh.mercik@gmail.com>
2025-04-01 22:18:40 -0400

    game constants, generate screen, and quit with close

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
+import pygame
+
+from constants import *
+
 def main():
-    import pygame
+    pygame.init
+    
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    while True:
+        #Let's people stop the game
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
+        screen.fill("black")
+
+        pygame.display.flip()
+    
 
 
 
