fdb8a274b1727fad7c63251a083896e0f0103ada
Josh Mercik <josh.mercik@gmail.com>
2025-04-01 22:28:42 -0400

    clock working at 60 fps

--- main.py
+++ main.py
@@ -3,13 +3,11 @@ import pygame
 from constants import *
 
 def main():
-    pygame.init
-    
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         #Let's people stop the game
@@ -20,7 +18,8 @@ def main():
         screen.fill("black")
 
         pygame.display.flip()
-    
+        clock.tick(60)
+        dt += 60/1000
 
 
 
