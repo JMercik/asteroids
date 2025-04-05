d37c64bf5c4d631f80853fa62b665e5d20e2f1be
Josh Mercik <josh.mercik@gmail.com>
2025-04-05 00:14:45 -0400

    asteroids spwan with random size and direction.

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -13,21 +15,25 @@ def main():
     #group list
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroid = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroid, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
 
     while True:
-        #Let's people stop the game
+        #Lets people stop the game
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
         screen.fill("black")
 
-        for thing in updatable:
-            thing.update(dt)
+        updatable.update(dt)
         
         for thing in drawable:
             thing.draw(screen)
